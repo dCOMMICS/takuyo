021cf3ff865175b5433c96dea228c4518a30c3c7
dCOMMICS <sanwurld@gmail.com>
2023-09-14 20:47:03 +0300

    initial (*) fixed code

--- prettier.config.js
+++ prettier.config.js
@@ -1,28 +1,28 @@
-// const options {
-//     arrowParens: 'avoid',
-//     singleOuoted: true,
-//     bracketSpacing:'true',
-//     endOfline:'lf',
-//     semi:  false,
-//     tabWidth:2,
-//     trailingComma: 'none',
+const options ={
+    arrowParens: 'avoid',
+    singleOuoted: true,
+    bracketSpacing:true,
+    endOfline:'lf',
+    semi:  false,
+    tabWidth:2,
+    trailingComma: 'none',
 
 
 
-// }
+}
 
-// module.exports =options
+module.exports =options
 
 // TODO://
 
-const options = {
-  arrowParens: 'avoid',
-  singleQuote: true,
-  bracketSpacing: true,
-  endOfLine: 'lf',
-  semi: false,
-  tabWidth: 2,
-  trailingComma: 'none'
-}
+// const options = {
+//   arrowParens: 'avoid',
+//   singleQuote: true,
+//   bracketSpacing: true,
+//   endOfLine: 'lf',
+//   semi: false,
+//   tabWidth: 2,
+//   trailingComma: 'none'
+// }
 
-module.exports = options
+// module.exports = options
