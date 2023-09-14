d2d67282a8f9f21efabd65f7dbdce5cc872e8197
dCOMMICS <sanwurld@gmail.com>
2023-09-14 20:41:53 +0300

    feat(*):Initial comit

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+// const options {
+//     arrowParens: 'avoid',
+//     singleOuoted: true,
+//     bracketSpacing:'true',
+//     endOfline:'lf',
+//     semi:  false,
+//     tabWidth:2,
+//     trailingComma: 'none',
+
+
+
+// }
+
+// module.exports =options
+
+// TODO://
+
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
